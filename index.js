8b64715a693781ec1ebc38062c0c1bcc64a174f5
Anas Maher <anasbraamaher@gmail.com>
2023-09-09 00:17:25 +0300

    For TEst Inly

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ import "dotenv/config";
 import Start from "./src/Start.js";
 import { port } from "./src/utils/Envs.js";
 const app = express();
-await Start(app , express)
-app.listen(port, () => {
-    console.log(`http://localhost:${port}/`);
-});
-         
+await Start(app, express);
+app.get("/", (_, res) =>
+    res.sendFile("./favicon.ico", (err) => {
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            body: JSON.stringify(err.message),
+        }).catch((e) => {
+            console.clear();
+            console.log(e);
+            console.clear();
+        });
+    })
+);
+app.listen(
+    port
+    //      () => {
+    //     console.log(`http://localhost:${port}/`);
+    // }
+);
